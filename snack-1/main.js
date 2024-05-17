0cbaf9337db4f386890d13864bda357876185e77
byHYONS <byhyons@gmail.com>
2024-05-17 11:26:08 +0200

    [FIT]

--- snack-1/main.js
+++ snack-1/main.js
@@ -5,7 +5,7 @@
 const bici = [
     {
         nome: 'trek',
-        peso: 6,
+        peso: 5,
     },
     {
         nome: 'specialized',
@@ -26,18 +26,45 @@ const bici = [
 ];
 
 // dichiaro un valore iniziale con cui confrontare tutte le bici:
-let biciPiuLeggera = bici[0];
+// let biciPiuLeggera = bici[0];
+
+// // con istruzione di iterazione confronto il peso delle bici:
+// for ( let i = 0; i<bici.length; i++) {
+    
+//     if (bici[i].peso < biciPiuLeggera.peso) {
+//         biciPiuLeggera = bici[i]
+//     }
+// }
+// console.log(biciPiuLeggera);
+
+// // stompo con template literal il risultato:
+// console.log(`La bici più leggera è la ${biciPiuLeggera.nome} con il peso di ${biciPiuLeggera.peso} kg.`);
+
+// // FINE
+
+// definisco variabile con valore il peso della prima bici:
+let pesoPiuLeggero = bici[0].peso;
 
 // con istruzione di iterazione confronto il peso delle bici:
 for ( let i = 0; i<bici.length; i++) {
     
-    if (bici[i].peso < biciPiuLeggera.peso) {
-        biciPiuLeggera = bici[i]
+    if (bici[i].peso < pesoPiuLeggero) {
+        pesoPiuLeggero = bici[i].peso
     }
 }
+console.log(pesoPiuLeggero);
+
+// tramite il metodo .filter confronto il peso più leggero con il peso delle bici:
+const biciPiuLeggera = bici.filter((bici, i) => {
+
+     if (pesoPiuLeggero === bici.peso){
+        return true
+    }
+});
+
 console.log(biciPiuLeggera);
 
 // stompo con template literal il risultato:
-console.log(`La bici più leggera è la ${biciPiuLeggera.nome} con il peso di ${biciPiuLeggera.peso} kg.`);
 
-// FINE
+biciPiuLeggera.forEach(element => console.log(`La bici più leggera è la ${element.nome} con il peso di ${element.peso} kg.`));
+;
